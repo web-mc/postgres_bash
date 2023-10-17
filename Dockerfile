5d9689c81d6dd83bbf65245d13350ff45943f47c
web-mc <peshkov.mc@gmail.com>
2023-10-17 13:09:05 +0300

    11

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM ubuntu:22.04
 
 RUN apt update && apt install lsb-core -y
+RUN apt install openssh-client nano -y
+# RUN apt install nano -y
+# RUN apt install systemctl -y
 
 CMD [ "/bin/bash" ]
--- app/init.sh
+++ app/init.sh
@@ -32,24 +32,31 @@ fi
 # Данные для PostgreSQL КОНЕЦ
 
 
-echo "$db_name $db_user $db_pass $db_port"
-
-
 # Добавляем юзера
-# if [ $(id -u) -eq 0 ]; then
-# 	read -p "Enter username : " username
-# 	read -s -p "Enter password : " password
-# 	egrep "^$username" /etc/passwd >/dev/null
-# 	if [ $? -eq 0 ]; then
-# 		echo "User $username already exists!"
-# 		exit 1
-# 	else
-# 		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-# 		useradd -m -p "$pass" "$username"
-# 		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-# 	fi
-# else
-# 	echo "Only root may add a user to the system."
-# 	exit 2
-# fi
-# # Добавляем юзера конец
+if [ $(id -u) -eq 0 ]; then
+	read -p "Enter username : " username
+	read -s -p "Enter password : " password
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "User $username already exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p "$pass" "$username"
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Skipping useradd step."
+fi
+# Добавляем юзера конец
+
+
+
+
+# gpasswd -a $username sudo
+
+# nano /etc/ssh/sshd_config
+# nano /etc/ssh/ssh_config
+
+cat /etc/ssh/ssh_config
+
