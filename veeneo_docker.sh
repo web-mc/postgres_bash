20a7c98057c7d5953a2a365cc35f1968159b684d
web-mc <peshkov.mc@gmail.com>
2023-10-23 15:38:59 +0300

    11111

--- veeneo_docker.sh
+++ veeneo_docker.sh
@@ -1,4 +1,4 @@
-# Стандартная установка докера
+# Стандартная установка докераа
 sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
